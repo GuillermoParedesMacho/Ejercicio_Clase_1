b1d62ef7728c8e6d9e7dfde304badf76fb849179
Guillermo Paredes Macho <guillermo_paredes_apps1ta1920@cev.com>
2020-10-02 17:38:30 +0200

    V0.11 	Añadir una segunda pantalla

---
+++ Ejercico_Clase_1/profileViewController.swift
@@ -0,0 +1,13 @@
+//
+//  profileViewController.swift
+//  Ejercico_Clase_1
+//
+//  Created by Apps2t on 02/10/2020.
+//  Copyright © 2020 Apps2t. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController:UIViewController{
+    
+}
