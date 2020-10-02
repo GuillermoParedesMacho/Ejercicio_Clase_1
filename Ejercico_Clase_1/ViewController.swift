0ec28ee82bcd62cf703b4b93cbaa8fa0fe3adf2c
Guillermo Paredes Macho <guillermo_paredes_apps1ta1920@cev.com>
2020-10-02 16:05:36 +0200

    Initial Commit

---
+++ Ejercico_Clase_1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ejercico_Clase_1
+//
+//  Created by Apps2t on 02/10/2020.
+//  Copyright © 2020 Apps2t. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
